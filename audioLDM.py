63ea5324e67ea6d9b5a21f42af7e1be83c53040e
mariasanruiz <62425347+mariasanruiz@users.noreply.github.com>
2023-07-12 18:04:06 +0200

    AudioLDM.py

--- audioLDM.py
+++ audioLDM.py
@@ -11,8 +11,7 @@ def generate_audio(captions_list):
     repo_id = "cvssp/audioldm-s-full-v2"
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, device=device)
-    #pipe = pipe.to("cuda")
-    #device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+
     for i, caption in enumerate(captions_list):
         audio = pipe(caption, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
         file_name = f"audio_generated_{i}.wav"
